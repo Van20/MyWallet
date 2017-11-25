374d53c7f2f05686a54159b5492b13736b2ff22b
Rizvan <rizvanariyanto@gmail.com>
2017-11-25 21:04:00 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/mywallet/splash_screen/Splash_activity.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.mywallet.splash_screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.sch.smktelkom_mlg.mywallet.R;
+
+public class Splash_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activity);
+    }
+}
