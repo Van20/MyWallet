48c8a4fcc3d55c672a9cb2061b40f8a0d5d9f9af
Rizvan <rizvanariyanto@gmail.com>
2017-11-25 22:06:31 +0700

    welcome screen + splash screen

---
+++ app/src/main/java/id/sch/smktelkom_mlg/mywallet/splash_screen/IncompatibleRatioException.java
@@ -0,0 +1,10 @@
+package id.sch.smktelkom_mlg.mywallet.splash_screen;
+
+public class IncompatibleRatioException extends RuntimeException {
+
+    private static final long serialVersionUID = 234608108593115395L;
+
+    public IncompatibleRatioException() {
+        super("Can't perform Ken Burns effect on rects with distinct aspect ratios!");
+    }
+}
